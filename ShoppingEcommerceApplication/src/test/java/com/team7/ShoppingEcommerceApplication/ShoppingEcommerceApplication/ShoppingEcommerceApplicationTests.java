56e1840a5afb8192ee626cf2865b0ebfd340960e
BalaMurali R <121211875+Balamurali03@users.noreply.github.com>
2024-06-18 12:44:38 +0530

    Add files via upload

---
+++ ShoppingEcommerceApplication/src/test/java/com/team7/ShoppingEcommerceApplication/ShoppingEcommerceApplication/ShoppingEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.ShoppingEcommerceApplication.ShoppingEcommerceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
